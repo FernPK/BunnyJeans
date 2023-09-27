c186fbb358ce32ce5fbfda937e4f078d103d3cc8
FernPK <pornnapat5563@gmail.com>
2023-09-27 11:04:56 +0700

    add types

---
+++ src/types.tsx
@@ -0,0 +1,37 @@
+export type Item = {
+  id: string,
+  name: string,
+  price: number,
+  sold: number,
+  stock: number,
+  reviews: number,
+  rating: number,
+  image: string[],
+  animal: string,
+  color: string,
+  tags: string[],
+}
+
+export type Collections = {
+  colName: string,
+  image: string,
+  route: string,
+  desc: string,
+}
+
+export type CartItemType = {
+  id: string,
+  name: string,
+  price: number,
+  stock: number,
+  image: string,
+  color: string,
+  amount: number,
+}
+
+export type RecentlyViewedType = {
+  id: string,
+  name: string,
+  price: number,
+  image: string,
+}
