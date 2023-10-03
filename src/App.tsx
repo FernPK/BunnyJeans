69cdc3864636191ec5912b2ff42a4327c626dbb5
FernPK <pornnapat5563@gmail.com>
2023-10-03 12:33:26 +0700

    move first loading to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,45 @@ import { Outlet } from 'react-router-dom'
 import './App.css'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
+import toast, { Toaster } from 'react-hot-toast';
+import { useEffect } from 'react';
 
 function App() {
 
+  let firstRender = false;
+
+  const startWebService = async () => {
+    toast.promise(
+      fetch('https://bunnyjeans-api.onrender.com/',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+          }
+      }
+      )
+        .then(function(response){
+          // console.log(response)
+          firstRender = true;
+          return response.json()
+        }),
+       {
+         loading: 'Starting web service...',
+         success: <b>Service is started</b>,
+         error: <b>Something error</b>,
+       }
+     );
+    
+  }
+
+  useEffect(() => {
+    if (firstRender) return;
+    startWebService()
+  }, [])
+
   return (
     <>
+      <Toaster />
       <Navbar />
       <Outlet />
       <Footer />
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,51 +1,9 @@
-import toast, { Toaster } from "react-hot-toast"
 import CollectionsHome from "../components/CollectionsHome"
 import Event from "../components/Event"
-import { useEffect } from "react";
-
-// function useFirstRender() {
-//   const ref = useRef(true);
-//   const firstRender = ref.current;
-//   ref.current = false;
-//   return firstRender;
-// }
-
 const Home = () => {
-
-  let firstRender = false;
-
-  const startWebService = async () => {
-    toast.promise(
-      fetch('https://bunnyjeans-api.onrender.com/',
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-          }
-      }
-      )
-        .then(function(response){
-          // console.log(response)
-          firstRender = true;
-          return response.json()
-        }),
-       {
-         loading: 'Starting web service...',
-         success: <b>Service is started</b>,
-         error: <b>Something error</b>,
-       }
-     );
-    
-  }
-
-  useEffect(() => {
-    if (firstRender) return;
-    startWebService()
-  }, [firstRender])
 
   return (
     <div>
-      <Toaster />
       <Event />
       <CollectionsHome />
     </div>
